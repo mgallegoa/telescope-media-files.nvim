921cf7b73b8185662d03907c1d98628f037d199e
mgallegoa <manuelgallego2003@yahoo.com>
2024-09-05 00:07:37 -0500

    Add support to visualice the image in external program
    Currently con the key-map Ctrl+i it open the image with an external program. By default is eog, but it could be other program configurable in the new attribute command_open_imen, if the att exist, the image is open with the tool send; this allow the flexibility to open the image in differents programs accourding to each machine.

--- lua/telescope/_extensions/media_files.lua
+++ lua/telescope/_extensions/media_files.lua
@@ -20,24 +20,25 @@ local M = {}
 local filetypes = {}
 local find_cmd = ""
 local image_stretch = 250
+local command_open_imen = "eog"
 
-M.base_directory=""
+M.base_directory = ""
 M.media_preview = defaulter(function(opts)
   return previewers.new_termopen_previewer {
-    title="Thumbnails",
+    title = "Thumbnails",
     get_command = opts.get_command or function(entry)
-      local tmp_table = vim.split(entry.value,"\t");
+      local tmp_table = vim.split(entry.value, "\t");
       local preview = opts.get_preview_window()
       opts.cwd = opts.cwd and vim.fn.expand(opts.cwd) or vim.loop.cwd()
       if vim.tbl_isempty(tmp_table) then
-        return {"echo", ""}
+        return { "echo", "" }
       end
       return {
-        M.base_directory .. '/scripts/vimg' ,
+        M.base_directory .. '/scripts/vimg',
         string.format([[%s/%s]], opts.cwd, tmp_table[1]),
-        preview.col ,
-        preview.line + 1 ,
-        preview.width ,
+        preview.col,
+        preview.line + 1,
+        preview.width,
         preview.height,
         image_stretch
       }
@@ -51,14 +52,14 @@ function M.media_files(opts)
       'find',
       '.',
       '-iregex',
-      [[.*\.\(]]..table.concat(filetypes,"\\|") .. [[\)$]]
+      [[.*\.\(]] .. table.concat(filetypes, "\\|") .. [[\)$]]
     },
     fd = {
       'fd',
       '--type',
       'f',
       '--regex',
-      [[.*.(]]..table.concat(filetypes,"|") .. [[)$]],
+      [[.*.(]] .. table.concat(filetypes, "|") .. [[)$]],
       '.'
     },
     fdfind = {
@@ -66,50 +67,58 @@ function M.media_files(opts)
       '--type',
       'f',
       '--regex',
-      [[.*.(]]..table.concat(filetypes,"|") .. [[)$]],
+      [[.*.(]] .. table.concat(filetypes, "|") .. [[)$]],
       '.'
     },
     rg = {
       'rg',
       '--files',
       '--glob',
-      [[*.{]]..table.concat(filetypes,",") .. [[}]],
+      [[*.{]] .. table.concat(filetypes, ",") .. [[}]],
       '.'
     },
   }
 
   if not vim.fn.executable(find_cmd) then
-    error("You don't have "..find_cmd.."! Install it first or use other finder.")
+    error("You don't have " .. find_cmd .. "! Install it first or use other finder.")
     return
   end
 
   if not find_commands[find_cmd] then
-    error(find_cmd.." is not supported!")
+    error(find_cmd .. " is not supported!")
     return
   end
 
   local sourced_file = require('plenary.debug_utils').sourced_filepath()
   M.base_directory = vim.fn.fnamemodify(sourced_file, ":h:h:h:h")
   opts = opts or {}
-  opts.attach_mappings= function(prompt_bufnr,map)
-    actions.select_default:replace(function()
-      local entry = action_state.get_selected_entry()
-      actions.close(prompt_bufnr)
-      if entry[1] then
-        local filename = entry[1]
-        vim.fn.setreg(vim.v.register, filename)
-        vim.notify("The image path has been copied!")
+  opts.attach_mappings = function(_, map)
+    local selection
+
+    map('n', '<C-i>', function()
+      selection = action_state.get_selected_entry()
+      local comand_open_imagen = command_open_imen .. ' "' .. selection.value .. '"'
+      local success = os.execute(comand_open_imagen)
+      if not success or success > 0 then
+        vim.notify("Error excecuting the command: " .. comand_open_imagen, vim.log.levels.ERROR)
       end
+    end)
+
+    map('n', '<CR>', function(prompt_bufnr)
+      selection = action_state.get_selected_entry()
+      actions.close(prompt_bufnr)
+      vim.fn.setreg(vim.v.register, selection.value)
+      vim.notify("The image path has been copied!" .. selection.value)
     end)
     return true
   end
   opts.path_display = { "shorten" }
 
-  local popup_opts={}
-  opts.get_preview_window=function ()
+  local popup_opts = {}
+  opts.get_preview_window = function()
     return popup_opts.preview
   end
-  local picker=pickers.new(opts, {
+  local picker = pickers.new(opts, {
     prompt_title = 'Media Files',
     finder = finders.new_oneshot_job(
       find_commands[find_cmd],
@@ -128,12 +137,12 @@ function M.media_files(opts)
   picker:find()
 end
 
-
 return require('telescope').register_extension {
   setup = function(ext_config)
-    filetypes = ext_config.filetypes or {"jpg", "png", "jpeg", "webm", "gif", }
+    filetypes = ext_config.filetypes or { "jpg", "png", "jpeg", "webm", "gif", }
     find_cmd = ext_config.find_cmd or "fd"
     image_stretch = ext_config.image_stretch or 250
+    command_open_imen = ext_config.command_open_imen or command_open_imen
   end,
   exports = {
     media_files = M.media_files
